1e2759678f23d434add015e6e5e2273494a22012
Rafael Damasceno <leafarmd@gmail.com>
2018-04-13 10:28:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 var app = require('express').createServer();
 var pos = 0;
 app.get('/',function(req,res) {
-	if(pos == 0) {
-	  pos = 1;
-	} else {
-	  pos = 0;
-	}
-	res.send(""+pos);
+	pos = pos + 1;
+	res.send("Hello World" + pos);
 });
 
 app.listen(process.env.PORT || 3000,function(){
