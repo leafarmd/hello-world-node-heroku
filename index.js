724b0e30504e60a9088748fd272fb72188a84dd6
Rafael Damasceno <rafael.damaceno@cwi.com.br>
2018-04-13 13:25:56 -0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var app = require('express').createServer();
 var cont = 0;
 app.get('/',function(req,res) {
-	cont++;
+	var id = req.query.id;
+	if(id == 1) {
+		cont = 1;
+	}
+	if(id == 2) {
+		cont = 2;
+	}
 	res.send("hello world" + cont);
 });
 
