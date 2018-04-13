aec2e8594a09596e211bbffa1f2815700d883217
Rafael Damasceno <leafarmd@gmail.com>
2018-04-13 13:13:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var app = require('express').createServer();
-app.get('/',function(req,res) {
-	res.send("Hello World");
+var cont = 0;
+app.get('/api',function(req,res) {
+	res.send(cont);
 });
 
 app.listen(process.env.PORT || 3000,function(){
 	console.log("listening on 3000");
 });
+
