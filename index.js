0efdaa0ae8b2f5ce4ad0b32bde9c28056786911a
Rafael Damasceno <rafael.damaceno@cwi.com.br>
2018-04-13 13:20:31 -0300

    update

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var app = require('express').createServer();
 var cont = 0;
 app.get('/',function(req,res) {
-	res.send(cont);
+	res.send("hello world");
 });
 
 app.listen(process.env.PORT || 3000,function(){
 	console.log("listening on 3000");
 });
-
