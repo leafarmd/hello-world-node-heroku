ad501e38277613d3b8e9dd90df344e6abbca6550
Rafael Damasceno <leafarmd@gmail.com>
2018-04-13 11:27:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var app = require('express').createServer();
 var resp = 0;
 app.get('/api',function(req,res) {
 	var id = req.query.id;
-	if (id == 0) {
+	if (id == "0") {
 	   res.send(resp);
 	}
-	if (id == 1) {
+	if (id == "1") {
 	   resp = 1;
 	}
-	if (id == 2) {
+	if (id == 2") {
 	  resp = 0;
 	}
 });
