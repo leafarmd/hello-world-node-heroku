51a969b776bddfe320e7f979f83c8c160f6a11b8
Rafael Damasceno <leafarmd@gmail.com>
2018-04-30 12:11:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/',function(req,res) {
 	if(id == 0) {
 		status = "desligado";
 	}
-	res.send("" + status);
+	res.send("<" + status + ">");
 });
 
 app.listen(process.env.PORT || 3000,function(){
